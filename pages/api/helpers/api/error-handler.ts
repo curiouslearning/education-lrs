87f3221ef5a27e1e98d2aaff7503f3a8c86143a0
Carlos Chinchilla <calufa@gmail.com>
2022-01-26 19:38:29 -0600

    fix typo

--- pages/api/helpers/api/error-handler.ts
+++ pages/api/helpers/api/error-handler.ts
@@ -9,7 +9,7 @@ function errorHandler (
   if (typeof(err) === 'string') {
     const is404 = err.toLowerCase().endsWith('not found');
     const statusCode = is404? 404:400;
-    return res.stsatus(statusCode).json({message: err})
+    return res.status(statusCode).json({message: err})
   }
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ message: 'Invalid Token'});
